9b785277d1c2ba0036656b2d44ccea9fe4c92cbf
m1n199honey <m1n199ada@gmail.com>
2023-08-12 01:24:43 +0000

    test 1.1.0

--- demo SJ/dashboard.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const dashboard = express.Router();
-
-dashboard.post("/dashboard", (req, res) => {
-    res.end("welcome to dashbord !");
-});
-module.exports = dashboard;
---
+++ test/src/main.tsx
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom";
+import Home from "./app/page";
+import "./app/globals.css"
+
+// eslint-disable-next-line react/no-deprecated
+ReactDOM.render(<Home/>, document.getElementById("root"))
